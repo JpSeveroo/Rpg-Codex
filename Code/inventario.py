46ec4cfe56dcba75542f26589ac9264c7305d587
André Felipe <andrefelipealecrim@gmail.com>
2025-06-26 17:00:37 -0300

    inventario 1.0

--- Code/inventario.py
+++ Code/inventario.py
@@ -1,4 +1,14 @@
+import utills
+
 class item :
     def __init__(self):
         self.nome = ''
         self.efeitos = ''
+
+def add_item():
+    a = input('Nome: ')
+    b = input('Efeito: ')
+    
+
+if __name__ == '__main__':
+    add_item()
