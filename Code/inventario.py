1fd70a79553b5e6cfa61f88ea39cab8b482bc02e
André Felipe <andrefelipealecrim@gmail.com>
2025-07-07 16:19:56 -0300

    Inventário 2.0

--- Code/inventario.py
+++ Code/inventario.py
@@ -1,26 +1,26 @@
 import item
 from InquirerPy import inquirer
 from ficha import Personagem
-from interface import personagem_escolhido
 from os import system
 
 lista_de_nomes_de_itens = [i.nome for i in item.lista_itens]
 
 class Equipamento:
     def __init__(self):
-        self.itens = { 'cabeca' : {
+        self.itens = { 
+        'Cabeça' : {
             'equipado' : False,
             'item' : ''
         },
-        'corpo' : {
+        'Corpo' : {
             'equipado' : False,
             'item' : ''
         },
-        'pes' : {
+        'Pés' : {
             'equipado' : False,
             'item' : ''
         },
-        'maos' : {
+        'Mãos' : {
             'equipado' : False,
             'item' : ''
         } 
@@ -31,35 +31,47 @@ equipamento = Equipamento()
 def equips(a):
     print('-'*35)
     try:
-        print('|' + f'Cabeça : {a.cabeca['item'].nome}'.center(33) + '|')
+        print('|' + f'Cabeça: {a.itens['Cabeça']['item'].nome}'.center(33) + '|')
     except:
-        print('|' + 'Não há itens equipados na Cabeça!'.center(33) + '|')
+        print('|' + 'Cabeça: '.center(33) + '|')
     try:
-        print('|' + f'Corpo : {a.corpo['item'].nome}'.center(33) + '|')
+        print('|' + f'Corpo: {a.itens['Corpo']['item'].nome}'.center(33) + '|')
     except:
-        print('|' + 'Não há itens equipados no Corpo!'.center(33) + '|')
+        print('|' + 'Corpo: '.center(33) + '|')
     try:
-        print('|' + f'Pés : {a.pes['item'].nome}'.center(33) + '|')
+        print('|' + f'Pés: {a.itens['Pés']['item'].nome}'.center(33) + '|')
     except:
-        print('|' + 'Não há itens equipados nos Pés!'.center(33) + '|')
+        print('|' + 'Pés: '.center(33) + '|')
     try:
-        print('|' + f'Mãos : {a.maos['item'].nome}'.center(33) + '|')
+        print('|' + f'Mãos: {a.itens['Mãos']['item'].nome}'.center(33) + '|')
     except:
-        print('|' + 'Não há itens equipados nas Mãos!'.center(33) + '|')
+        print('|' + 'Mãos: '.center(33) + '|')
     print('-'*35)
 
 def interface_inv(personagem):
     system('clear')
     equips(equipamento)
-    opcoes = [
-        {'name':'Cabeça', 'value':'cabeca'},
-        {'name':'Corpo', 'value':'cabeca'}
-    ]
-    a = inquirer.select(message='Qual equipamento você deseja alterar ?', choices=opcoes).execute()
-    print(a)
+    personagem.equipamento = equipamento
+    a = inquirer.select(message='Qual equipamento você deseja alterar ?', choices=['Cabeça', 'Corpo', 'Pés', 'Mãos', 'Sair']).execute()
+    if a != 'Sair':
+        disponiveis = []
+        for i in personagem.inventario :
+            if i.categoria == a :
+                disponiveis.append(i)
+        if len(disponiveis) == 0 :
+            print('Não há nenhum equipamento disponivel para essa parte')
+            input('Pressione enter para voltar...')
+            interface_inv(personagem)
+        nomes_itens = []
+        for i in range(len(disponiveis)):
+            nomes_itens.append(disponiveis[i].nome)
+        b = inquirer.select(message=f'Qual o item que você deseja equipar na(o) {a}: ', choices=nomes_itens).execute()
+        equipamento.itens[a]['item'] = disponiveis[nomes_itens.index(b)]
+        interface_inv(personagem)
+    else:
+        pass
 
 if __name__ == '__main__':
-    item.load_itens()
     p = Personagem()
     p.inventario.append(item.lista_itens[4])
     p.inventario.append(item.lista_itens[5])
