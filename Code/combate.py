a033c6fb0a010de0e406f102adbe035b879f1f15
Carlos Souza Filho <cadu.dev99@gmail.com>
2025-07-09 05:01:58 -0300

    conserta a esquiva e o retorno ao inicio

--- Code/combate.py
+++ Code/combate.py
@@ -7,6 +7,7 @@ import utills
 import ficha
 import inimigos
 import inventario
+import interface
 
 """=== FUNÇÕES UTILITARIAS ==="""
 
@@ -243,8 +244,8 @@ def loop_principal(personagem, inimigo, mana_max):
             valor_acao = (a, a2, a3)
 
         elif a == 'Esquivar':
-            esquivar(personagem, mana_max)
-            continue
+            sucesso = esquivar(personagem, mana_max)
+            break
         
         elif a == 'Inventário':
             inv(personagem, mana_max)
@@ -286,6 +287,7 @@ def combate(personagem, inimigo):
         return personagem
     elif inimigo.vida_atual > 0:
         utills.digitar(f"\n💀 {personagem.nick} foi derrotado na torre...")
+        interface.interface.interface_principal()
         time.sleep(3)
         utills.limpar_tela()
         return inimigo
